ad65326ea44be05a7d4e9f0b6c18689e7e1d4c82
Leotamn <103520627+Leotamn@users.noreply.github.com>
2022-11-03 23:38:36 +0900

    チャレンジ
    完了

--- 1-3/main.js
+++ 1-3/main.js
@@ -1,16 +1,18 @@
+let a = 12;
+let b = 14;
 a != 10;
-b >= 10 && b <= 20 && b%2 !== 0
+b >= 10 && b <= 20 && b%2 === 0
 
 let x = 10;
-if (score >= 10 && score <= 20) {
+if (x >= 10 && x <= 20) {
     console.log('成功です');
 } else {
     console.log('失敗です');
 }
 
 let y = 0;
-if(y % 2 ===0) {
+if(y % 2 === 0) {
     console.log('偶数です');
 } else {
-    consoke.log('奇数です');
+    console.log('奇数です');
 }
