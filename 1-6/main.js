90cb5432ceefa3aa50c71bbb5b75ef9f7c6dfc2e
Leotamn <103520627+Leotamn@users.noreply.github.com>
2023-01-18 01:36:14 +0900

    あ
    作成

---
+++ 1-6/main.js
@@ -0,0 +1,13 @@
+let scores = [10, 15, 20, 25];
+for(i = 0; i < 25; i++) {
+    if(i % 2 == 0) {
+     console.log("得点は" + scores[i] + "点です");
+}}
+
+let gas = {
+  gass: 20.5,
+  num: 1234,
+};
+
+console.log("ガソリンは" + gas.gass + "です");
+console.log("ナンバーは" + gas.num + "です");
