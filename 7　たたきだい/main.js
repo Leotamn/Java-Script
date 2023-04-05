371dd7092d98c89657b713eec5ff15663e3b91d5
Leotamn <103520627+Leotamn@users.noreply.github.com>
2023-04-05 23:39:52 +0900

    a
    a

--- 7　たたきだい/main.js
+++ 7　たたきだい/main.js
@@ -1,11 +1,22 @@
 
-function isEven(numbers) {
-    return  numbers % 2 === 0;
-}
-
 let numbers = [2, 5, 12, 13, 15, 18, 22];
 
-if(isEven(numbers)) {
-console.log(numbers + 'は偶数です');
+
+function isEven(num) {
+    return num % 2 == 0;
 }
+
+function isEvenIncluded(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        let num = numbers[i];
+        if (isEven(num)) {
+            return true;
+        }
+    }
+    return false;
+}   
+
+console.log(num + 'は偶数です');
+
+
 
